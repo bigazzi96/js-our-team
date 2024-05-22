1c84b51b9d433422658672f58b57821f530a5938
bigazzi96 <fbigazzi96@gmail.com>
2024-05-22 18:36:51 +0200

    aggiunta funzione per stampare su console

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
 //* array con informazioni del team
 
- const Team =[ 
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        immagine: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        immagine: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        immagine: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        immagine: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        immagine: 'scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        immagine: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
 
-{
-nome:'Wayne Barnett',
-ruolo:'Founder & CEO',
-immagine:'wayne-barnett-founder-ceo.jpg',
-},
-{
-nome:'Angela Caroll',
-ruolo:'Chief Editor',
-immagine:'angela-caroll-chief-editor.jpg',
-},
+//* stampa informazioni su console
+team.forEach((socio) => {
+    console.log(`Nome: ${socio.nome}, Ruolo: ${socio.ruolo}, Immagine: ${socio.immagine}`);
+});
 
-{
- nome:'Walter Gordon'
- ruolo:'Office Manager',
- immagine:'walter-gordon-office-manager.jpg',
-},
-
-{
- nome:'Angela Lopez',
- ruolo:'Social Media Manager',
- immagine:'angela-lopez-social-media-manager.jpg',
-},
-
-{
- nome:'Scott Estrada',
- ruolo:'Developer',
- immagine:'scott-estrada-developer.jpg',
-},
-
-{
- nome:'Barbara Ramos',
- ruolo:'Graphic Designer',
- immagine:'barbara-ramos-graphic-designer.jpg',
-},];
